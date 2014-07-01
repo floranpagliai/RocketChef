cd15eee03a7e41b9f79397b8dc08c105b8bb6fb1
Floran Pagliai <floran.pagliai@gmail.com>
2014-07-01 12:32:52 +0200

    Ingredient add form

--- src/RocketChef/DataBundle/Form/Type/NewIngredientType.php
+++ src/RocketChef/DataBundle/Form/Type/NewIngredientType.php
@@ -1,26 +1,25 @@
 <?php
 /**
- * IngredientType.php
- * User: Utilisateur
- * Date: 02/06/14
- * Time: 16:59
+ * NewIngredientType.php
+ * User: floran.pagliai
+ * Date: 01/07/14
+ * Time: 11:49
  */
 
 namespace RocketChef\DataBundle\Form\Type;
 
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\Security\Core\SecurityContext;
 
-class IngredientType extends AbstractType
+class NewIngredientType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name', 'text', array('read_only' => true));
-        $builder->add('priceForUnit', 'money', array('read_only' => true));
-        $builder->add('unit', 'choice', array('choices' => array(0 => '/Unité', 1 => '/Kg', 2 => '/L'),
-                      'attr'=> array('disabled'=>'disabled')));
+        $builder->add('name', 'text');
+        $builder->add('priceForUnit', 'money');
+        $builder->add('unit', 'choice', array('choices' => array(0 => '/Unité', 1 => '/Kg', 2 => '/L')));
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
--- src/RocketChef/IngredientBundle/Controller/IngredientController.php
+++ src/RocketChef/IngredientBundle/Controller/IngredientController.php
@@ -9,7 +9,12 @@
 namespace RocketChef\IngredientBundle\Controller;
 
 
+use DateInterval;
+use DatePeriod;
+use RocketChef\DataBundle\Entity\Ingredient;
+use RocketChef\DataBundle\Form\Type\NewIngredientType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class IngredientController extends Controller
 {
@@ -20,5 +25,45 @@ class IngredientController extends Controller
         $paramsRender = array('ingredients' => $ingredients);
 
         return $this->render('RocketChefIngredientBundle:ingredient:list.html.twig', $paramsRender);
+    }
+
+    public function showAction($ingredientId)
+    {
+        $restaurant = $this->container->get('security.context')->getToken()->getUser()->getRestaurant();
+        $ingredient = $this->getDoctrine()->getRepository('RocketChefDataBundle:Ingredient')->find($ingredientId);
+
+        if ($ingredient && $ingredient->getRestaurant() == $restaurant) {
+            $paramsRender = array(
+                'ingredient' => $ingredient,
+            );
+        } else
+            throw $this->createNotFoundException('Ingredient introuvable');
+
+        return $this->render('RocketChefIngredientBundle:ingredient:show.html.twig', $paramsRender);
+    }
+
+    public function addAction(Request $request)
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(new NewIngredientType(), $ingredient);
+
+        if ($request->isMethod('POST')) {
+            $form->bind($request);
+            if ($form->isValid()) {
+                $ingredient = $form->getData();
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($ingredient);
+                $em->flush();
+                return $this->redirect($this->generateUrl('rocketchef_ingredient'));
+            }
+        }
+
+        $paramsRender = array('form' => $form->createView());
+        return $this->render('RocketChefIngredientBundle:ingredient:add.html.twig', $paramsRender);
+    }
+
+    public function editAction()
+    {
+
     }
 } 
